7c992047c75851b5770053fa66a51bb977471bc8
Md. Raisul Kabir Khan Likhon <114115052+likhon15-4915@users.noreply.github.com>
2023-02-19 02:58:48 +0600

    Add files via upload

---
+++ 1005.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    float A,B,x,y;
+   scanf("%f%f",&A,&B);
+   x=(A*3.5+B*7.5);
+  y=x/(3.5+7.5);
+   printf("MEDIA = %.5f\n",y);
+    return 0;
+}
---
+++ 2763.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    char num[100];
+    scanf("%s",&num);
+    printf("%c%c%c\n",num[0],num[1],num[2]);
+    printf("%c%c%c\n",num[4],num[5],num[6]);
+    printf("%c%c%c\n",num[8],num[9],num[10]);
+    printf("%c%c\n",num[12],num[13]);
+   return 0;
+}
